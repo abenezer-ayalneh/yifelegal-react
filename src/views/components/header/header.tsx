f01fccda5abb470cacc4b3097c512d66ee4b94c4
abenezer.ayalneh <abene42@gmail.com>
2022-12-07 18:50:36 +0300

    progress: working on the header

--- src/views/components/header/header.tsx
+++ src/views/components/header/header.tsx
@@ -62,7 +62,7 @@ export default function Header({onOpenNav}: { onOpenNav: () => void }) {
         return breadcrumbArray
     }, [location])
     return (
-        <StyledRoot>
+        <StyledRoot>`
             <StyledToolbar>
                 <IconButton
                     onClick={onOpenNav}
