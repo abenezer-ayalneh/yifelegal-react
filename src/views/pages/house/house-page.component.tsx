3048c53433672d4960af40fb92bffa1bea22327b
abenezer.ayalneh <abene42@gmail.com>
2022-12-08 19:25:12 +0300

    feat: apartment page done

--- src/views/pages/house/house-page.component.tsx
+++ src/views/pages/house/house-page.component.tsx
@@ -1,6 +1,33 @@
+import {Box, Grid, Stack, Typography} from "@mui/material";
+import HorizontalTile from "../../components/cards/horizontal-tile/horizontal-tile.component";
+import VerticalTile from "../../components/cards/vertical-tile/vertical-tile.component";
+import Apartment from "../../../assets/images/apartment.jpg";
+import Villa from "../../../assets/images/villa.jpg";
+import GroundPlus from "../../../assets/images/ground+.jpg";
+import RealEstate from "../../../assets/images/real-estate.jpg";
+import Condominium from "../../../assets/images/condominium.jpg";
+import Other from "../../../assets/images/other.jpg";
+import React from "react";
+
 const HousePage = () => {
     return (
-        <h1>House Page</h1>
+        <Box>
+            {/*Page Title*/}
+            <Stack justifyContent={"center"} alignItems={"center"} direction={"column"}>
+                <Typography variant={"h1"}>House</Typography>
+                <Typography variant={"subtitle2"}>Please choose the sub-category of what you are looking for</Typography>
+            </Stack>
+            <Box height={30}></Box>
+            {/*Tiles*/}
+            <Grid container paddingX={{xs: 5, md: 5, lg: 3, xl: 15}} justifyContent={{xs: "left", sm: "center", lg: "left"}} spacing={{xs: 1, sm: 2, md: 3}}>
+                <HorizontalTile tileTitle={"Apartment"} image={Apartment} to={"/request/house"}/>
+                <VerticalTile tileTitle={"Villa"} image={Villa} to={"/request/land"}/>
+                <VerticalTile tileTitle={"Ground+"} image={GroundPlus} to={"/request/commercial-building"}/>
+                <VerticalTile tileTitle={"RealEstate"} image={RealEstate} to={"/request/guest-house"}/>
+                <HorizontalTile tileTitle={"Condominium"} image={Condominium} to={"/request/machinery-and-truck"}/>
+                <VerticalTile tileTitle={"Other"} image={Other} to={"/request/car"}/>
+            </Grid>
+        </Box>
     )
 }
 
