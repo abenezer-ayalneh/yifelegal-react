825e2bca3fbcba9741fc9514bbb2934aa3a4e923
abenezer-ayalneh <abenezer.ayalneh.42@gmail.com>
2023-01-13 05:57:19 +0300

    feat: my request page done

--- src/views/components/page-dispatchers/my-request-list-page-dispatcher.component.tsx
+++ src/views/components/page-dispatchers/my-request-list-page-dispatcher.component.tsx
@@ -9,6 +9,11 @@ import {ENTITIES} from "../../../utils/const/entities";
 const HouseMyRequestsPage = Loadable(lazy(() => import("../../pages/my-requests/house-my-requests-page.component")))
 const LandMyRequestsPage = Loadable(lazy(() => import("../../pages/my-requests/land-my-requests-page.component")))
 const CommercialBuildingMyRequestsPage = Loadable(lazy(() => import("../../pages/my-requests/commercial-building-my-requests-page.component")))
+const GuestHouseMyRequestsPage = Loadable(lazy(() => import("../../pages/my-requests/guest-house-my-requests-page.component")))
+const MachineryAndTrucksMyRequestsPage = Loadable(lazy(() => import("../../pages/my-requests/machinery-and-trucks-my-requests-page.component")))
+const CarMyRequestsPage = Loadable(lazy(() => import("../../pages/my-requests/car-my-requests-page.component")))
+const ThreeWheelerMyRequestsPage = Loadable(lazy(() => import("../../pages/my-requests/three-wheeler-my-requests-page.component")))
+const WholeBuildingMyRequestsPage = Loadable(lazy(() => import("../../pages/my-requests/whole-building-my-requests-page.component")))
 
 const MyRequestListPageDispatcher = (): JSX.Element => {
     const {entity} = useParams<DispatcherPageParams>()
@@ -21,6 +26,16 @@ const MyRequestListPageDispatcher = (): JSX.Element => {
                 return <LandMyRequestsPage/>
             case ENTITIES.COMMERCIAL_BUILDING:
                 return <CommercialBuildingMyRequestsPage/>
+            case ENTITIES.GUEST_HOUSE:
+                return <GuestHouseMyRequestsPage/>
+            case ENTITIES.MACHINERY_AND_TRUCKS:
+                return <MachineryAndTrucksMyRequestsPage/>
+            case ENTITIES.CAR:
+                return <CarMyRequestsPage/>
+            case ENTITIES.THREE_WHEELER:
+                return <ThreeWheelerMyRequestsPage/>
+            case ENTITIES.WHOLE_BUILDING:
+                return <WholeBuildingMyRequestsPage/>
             default:
                 return <Error404Page/>
         }
