62f5b9474b1295ecc6da956af7c8719c5f437939
abenezer-ayalneh <abenezer.ayalneh.42@gmail.com>
2023-01-16 05:22:35 +0300

    feat: finalize my request page

---
+++ src/views/pages/my-requests/machinery-and-trucks-my-requests-page.component.tsx
@@ -0,0 +1,79 @@
+import {useParams} from "react-router-dom";
+import useFetch from "../../../utils/hooks/use-fetch";
+import config from "../../../config";
+import {Grid, Typography} from "@mui/material";
+import Highlight from "../../components/cards/highlight/highlight.component";
+import MachineryAndTrucks from "../../../assets/images/whole-building.jpg"
+import {useMemo, useState} from "react";
+import FullscreenLoadingAnimation from "../../components/fullscreen-loading-animation/fullscreen-loading-animation";
+import {RequestDetailType, RequestType} from "../../../utils/types/request-type";
+import moment from "moment"
+import CustomModal from "../../components/modal/modal.component";
+import FormRow from "../../components/form-row/form-row.component";
+import {useTranslation} from "react-i18next";
+import EmptyComponent from "../../components/empty/empty.component";
+import {DispatcherPageParams} from "../../../utils/types/dispatcher-page-param-types";
+
+const MachineryAndTrucksMyRequestsPage = () => {
+    const {t} = useTranslation()
+    const params = useParams<DispatcherPageParams>()
+    const [selectedMachineryAndTrucksDetail, setSelectedMachineryAndTrucksDetail] = useState<RequestDetailType[] | null>(null)
+    const {responseData, isRequestLoading} = useFetch({
+        method: "GET",
+        url: config.REACT_APP_ROOT_URL + `request/mine/entity`,
+        params: {
+            entity: params.entity
+        }
+    }, "fetch-my-whole-building-requests-on-whole-building-my-request-page")
+
+    const myRequests = useMemo<RequestType[]>(() => responseData?.data?.myRequests, [responseData])
+
+    const handleModalClose = () => setSelectedMachineryAndTrucksDetail(null)
+
+    const handleHighlightClicked = (request: RequestDetailType[]) => setSelectedMachineryAndTrucksDetail(request)
+
+    if (!myRequests) {
+        return <FullscreenLoadingAnimation/>
+    } else {
+        return (
+            <>
+                <Grid container spacing={2} padding={2}>
+                    {
+                        isRequestLoading
+                            ? <FullscreenLoadingAnimation/>
+                            : (myRequests.length === 0
+                                    ? <EmptyComponent/>
+                                    : myRequests.map((request: RequestType) => (
+                                        <Highlight image={MachineryAndTrucks}
+                                                   type={"Machinery & Trucks"}
+                                                   date={moment(request.created_at)?.format("MMM DD, YYYY")}
+                                                   deal={request.detail?.find((detail) => detail.attribute === "deal")?.value}
+                                                   location={request.detail?.find((detail) => detail.attribute === "subCity")?.value}
+                                                   category={request.detail?.find((detail) => detail.attribute === "entityType")?.value}
+                                                   onClick={() => handleHighlightClicked(request.detail)}
+                                        />)
+                                    )
+                            )
+                    }
+                </Grid>
+                {
+                    selectedMachineryAndTrucksDetail &&
+                    <CustomModal open={Boolean(selectedMachineryAndTrucksDetail)} title={"Request Detail"} handleClose={handleModalClose}>
+                        <Grid container direction="column" rowSpacing={1} paddingX={{xs: 1, sm: 2, md: 3, lg: 4}} paddingY={2}>
+                            {
+                                selectedMachineryAndTrucksDetail.map((request) => (
+                                    <FormRow label={t(request.attribute) + ":"}>
+                                        <Typography minWidth={200} sx={{textTransform: "capitalize"}}
+                                                    variant={"body2"}>{request.value}</Typography>
+                                    </FormRow>
+                                ))
+                            }
+                        </Grid>
+                    </CustomModal>
+                }
+            </>
+        )
+    }
+}
+
+export default MachineryAndTrucksMyRequestsPage
